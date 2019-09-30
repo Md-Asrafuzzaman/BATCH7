be2e4c81ef65881abd878bdc3c36f9ccce761daf
jaman <jamanswe95@gmail.com>
2019-09-30 20:33:31 +0600

    First Commit for three layer architecture

---
+++ MyWindowsFormsApp/MyWindowsFormsApp/BLL/IteamManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using MyWindowsFormsApp.Repository;
+
+namespace MyWindowsFormsApp.BLL
+{
+    public class IteamManager
+    {
+        IteamRepository _iteamRepository = new IteamRepository();
+        public bool Add(string name, double price)
+        {      
+            return _iteamRepository.Add(name, price);
+        }
+
+        public bool IsNameExists(string name)
+        {
+            return _iteamRepository.IsNameExists(name);
+        }
+
+        public bool Delete(int id)
+        {
+            return _iteamRepository.Delete(id);
+        }
+
+        public bool Update(string name, double price, int id)
+        {
+            return _iteamRepository.Update(name, price, id);
+        }
+
+        public DataTable Display()
+        {
+            return _iteamRepository.Display();
+        }
+
+        public DataTable Search(string name)
+        {
+            return _iteamRepository.Search(name);
+        }
+    }
+}
---
+++ MyWindowsFormsApp/MyWindowsFormsApp/ItemUi.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyWindowsFormsApp.BLL;
+
+namespace MyWindowsFormsApp
+{
+    public partial class ItemUi : Form
+    { 
+        IteamManager _iteamManager = new IteamManager();
+         public ItemUi()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            //Check UNIQUE
+            if (_iteamManager.IsNameExists(nameTextBox.Text))
+            {
+                MessageBox.Show(nameTextBox.Text + " Already Exists!");
+                return;
+            }
+
+            //Set Price as Mandatory
+            if (String.IsNullOrEmpty(priceTextBox.Text))
+            {
+                MessageBox.Show("Price Can not be Empty!!!");
+                return;
+            }
+
+            //Add/Insert Item
+            bool isAdded = _iteamManager.Add(nameTextBox.Text, Convert.ToDouble(priceTextBox.Text));
+
+            if (isAdded)
+            {
+                MessageBox.Show("Saved");
+            }
+            else
+            {
+                MessageBox.Show("Not Saved");
+            }
+
+            showDataGridView.DataSource =_iteamManager.Display();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            showDataGridView.DataSource = _iteamManager.Display();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+
+            //Set Id as Mandatory
+            if (String.IsNullOrEmpty(idTextBox.Text))
+            {
+                MessageBox.Show("Id Can not be Empty!!!");
+                return;
+            }
+
+            //Delete
+            if (_iteamManager.Delete(Convert.ToInt32(idTextBox.Text)))
+            {
+                MessageBox.Show("Deleted");
+            }
+            else
+            {
+                MessageBox.Show("Not Deleted");
+            }
+
+            showDataGridView.DataSource = _iteamManager.Display();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            showDataGridView.DataSource = _iteamManager.Search(nameTextBox.Text);
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            //Set Id as Mandatory
+            if (String.IsNullOrEmpty(idTextBox.Text))
+            {
+                MessageBox.Show("Id Can not be Empty!!!");
+                return;
+            }
+            //Set Price as Mandatory
+            if (String.IsNullOrEmpty(priceTextBox.Text))
+            {
+                MessageBox.Show("Price Can not be Empty!!!");
+                return;
+            }
+
+            if (_iteamManager.Update(nameTextBox.Text, Convert.ToDouble(priceTextBox.Text), Convert.ToInt32(idTextBox.Text)))
+            {
+                MessageBox.Show("Updated");
+                showDataGridView.DataSource = _iteamManager.Display();
+            }
+            else
+            {
+                MessageBox.Show("Not Updated");
+            }
+        }
+        //Method
+    }
+}
